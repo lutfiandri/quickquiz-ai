35f7159c463a8ca3ac2bf25a52f30dece399a4f1
lutfiandri <lutfiandri232@gmail.com>
2025-03-05 13:48:30 +0700

    feat: setup sveltekit with daisyui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div data-theme="cupcake" class="min-h-screen">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="container mx-auto flex min-h-screen flex-col items-center justify-center gap-8">
+	<div class="flex flex-col items-center gap-2">
+		<h1 class="text-6xl font-black">QuickQuiz AI</h1>
+		<p class="text-xl">Turn your PDFs into a Quiz—instantly!</p>
+	</div>
+
+	<div>Dropzone</div>
+
+	<div class="btn btn-primary font-bold">Generate Quiz!</div>
+</div>
